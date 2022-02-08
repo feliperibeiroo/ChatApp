f6523e2acd5e4c0ccb03eef1937211ec301d16c1
Felipe Ribeiro <felipebezerra35@gmail.com>
2022-02-08 13:56:12 -0300

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-button @click="enviarMsg" variant="outline-primary">Button</b-button>
+</template>
+
+<script>
+import { io } from "socket.io-client";
+export default {
+  data(){
+    return {
+      socket: io("http://127.0.0.1:8080", { transports : ['websocket'] })
+    }
+  },
+  methods: {
+    enviarMsg() {
+      this.socket.emit('msg', 'Oi servidor!')
+    }
+  }
+}
+</script>
